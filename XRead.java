f6e1f4471f045bfe388541058306c85bd9a4f248
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2020-01-19 17:28:26 +0100

    fix

--- XRead.java
+++ XRead.java
@@ -2,17 +2,14 @@ package exam_state;
 
 public class XRead implements State {
 
-    private MessageHandler mh;	// XXX dieses Feld wird im XRead gar nicht verwendet, d.h. man könnte dieses Feld auch weglassen.
-    public int x;	// XXX ich würde beide Felder, mh und x als final deklarieren.
+    private final int x;
 
     public XRead(MessageHandler mh, int x) {
-        this.mh = mh;
         this.x = x;
     }
 
     @Override
     public State handle(MessageHandler mh, String msg) {
-        return new YRead(mh, Integer.parseInt(msg));
-        // XXX da fehlt mir jetzt aber das x, denn dieses geht verloren, oder? Das sollte ebenfalls dem YRead übergeben werden.
+        return new YRead(mh, x, Integer.parseInt(msg));
     }
 }
--- YRead.java
+++ YRead.java
@@ -2,21 +2,19 @@ package exam_state;
 
 public class YRead implements State {
 
-    public int x;
-    public int y;
-    public MessageHandler mh;
+    private final int x;
+    private final int y;
+    private final MessageHandler mh;
 
-    public YRead(MessageHandler mh, int y) {
+    public YRead(MessageHandler mh, int x, int y) {
         this.mh = mh;
+        this.x = x;
         this.y = y;
     }
 
     @Override
     public State handle(MessageHandler mh, String msg) {
         this.mh.getPublisher().newData(x, y, Integer.parseInt(msg));
-        // XXX genau, aber das x wird nicht initialisiert. Wenn Sie die Felder x, y und mh final deklariert hätten, dann 
-        //     wäre dieser Fehler "aufgeflogen". Sie müssen also im YRead-Konstruktor noch das Feld x vorsehen, und
-        //     in der KLasse XRead wird dann das gespeicherte x dem Konstruktor von YRead übergeben.
         return new Init();
     }
 }
